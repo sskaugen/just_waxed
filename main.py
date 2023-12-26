335156b619ccd586bfa904766f2627508f016da1
sskaugen <153758234+sskaugen@users.noreply.github.com>
2023-12-25 23:23:31 -0600

    updated the math logic to accept multiple commis

--- main.py
+++ main.py
@@ -1,10 +1,11 @@
 from Employee import Employee
 from mathmodule import mathlogic
-#def __init__(self, name, hourly_rate,commission,start_date,birth_date):
+#def __init__(self, name, hourly_rate,commission_services,commision_products,start_date,birth_date):
 def main():
-    emp1 = Employee('Ximena',float(15),float(.07),"december 15 2022","march 23 2001")
+    emp1 = Employee('Ximena',float(15),float(.07),float(.10),"december 15 2022","march 23 2001")
     mathforemp1 = mathlogic()
-    mathforemp1.total_made_commison_from_services(emp1,emp1.get_commission(),565.50)
+    mathforemp1.total_made_commison_from_services(emp1,565.50)
+    print(mathforemp1.total_made_commissom_from_products(emp1,100))
 
 #I want to keep this change
 if __name__ == "__main__":
--- mathmodule.py
+++ mathmodule.py
@@ -10,15 +10,14 @@ class mathlogic():
     
     #Parametrs: self,Employee,Commison_rate,total_$_amount_services
     #returns: the total amount to be paid to the employee in commisson
-    def total_made_commison_from_services(self,employee,Commisson_rate,total_amount_services):
-        commisonRate = Employee.get_commission(employee)
-        commison_to_paid = round(total_amount_services * commisonRate,2)
-        #print(commison_to_paid) for debugging
-        return commison_to_paid
+    def total_made_commison_from_services(self,employee,total_amount_services):
+        commison_rate_services = Employee.get_commission_services(employee)
+        commison_to_pay_services = round(total_amount_services * commison_rate_services,2)
+        return commison_to_pay_services
     
     #Parametrs: self,Employee,Commison_rate_products,total_$_amount_products
     #returns: the total amount to be paid to the employee in commisson for products
-    def total_made_commissom_from_products(self,employee,Commisson_rate,total_amount_products):
-        commisonRateProducts = Employee.get_commission_products(employee)
-        commison_to_paid_products = round(total_amount_products * commisonRateProducts,2)
-        return commison_to_paid_products
+    def total_made_commissom_from_products(self,employee,total_amount_products):
+        commison_rate_products = Employee.get_commission_products(employee)
+        commison_to_pay_products = round(total_amount_products * commison_rate_products,2)
+        return commison_to_pay_products
