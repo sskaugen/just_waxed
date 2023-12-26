585b3ed77affe17b82b94b361b4b8670ee6bd8cf
sskaugen <153758234+sskaugen@users.noreply.github.com>
2023-12-26 00:21:53 -0600

    fixed the final weekly pay calculation

--- main.py
+++ main.py
@@ -3,11 +3,15 @@ from mathmodule import mathlogic
 #def __init__(self, name, hourly_rate,commission_services,commision_products,start_date,birth_date):
 def main():
     emp1 = Employee('Ximena',float(15),float(.07),float(.10),"december 15 2022","march 23 2001")
-    mathforemp1 = mathlogic()
-    mathforemp1.total_made_commison_from_services(emp1,565.50)
-    print(mathforemp1.total_made_commissom_from_products(emp1,100))
+    print(mathlogic.total_made_commison_from_services(emp1,100))
     print()
-    print(mathforemp1.total_from_weekly_pay(emp1,10)) #expecting 150
+    print(mathlogic.total_made_commissom_from_products(emp1,100))
+    print()
+    print(mathlogic.total_from_weekly_pay(emp1,10)) #expecting 150
+    
+    emp2 = Employee('Sean',float(15),float(.07),float(.10),"december 15 2022","march 23 2001")
+    print(mathlogic.final_weekly_pay(emp2,10,100,100))#expecting 167
+
 
 #I want to keep this change
 if __name__ == "__main__":
--- mathmodule.py
+++ mathmodule.py
@@ -3,30 +3,42 @@ class mathlogic():
     #constructor to hold the math logic
     def __init__(self):
         self.__weekpay = 0
-
-
-    #def weekly_pay(self,employee,hourly_rate,hours_worked,commisson_rate,totalamount):
-        #return()
-    
+        
     #Parametrs: self,Employee,Commison_rate,total_$_amount_services
     #returns: the total amount to be paid to the employee in commisson
-    def total_made_commison_from_services(self,employee,total_amount_services):
-        commison_rate_services = Employee.get_commission_services(employee)
+    @staticmethod
+    def total_made_commison_from_services(employee,total_amount_services):
+        commison_rate_services = employee.get_commission_services()
         commison_to_pay_services = round(total_amount_services * commison_rate_services,2)
         return commison_to_pay_services
     
     #Parametrs: self,Employee,,total_$_amount_products
     #returns: the total amount to be paid to the employee in commisson for products
-    def total_made_commissom_from_products(self,employee,total_amount_products):
-        commison_rate_products = Employee.get_commission_products(employee)
+    @staticmethod
+    def total_made_commissom_from_products(employee,total_amount_products):
+        commison_rate_products = employee.get_commission_products()
         commison_to_pay_products = round(total_amount_products * commison_rate_products,2)
         return commison_to_pay_products
     
     #Parametrs: self,Employee,total_hours_worked
     #returns: the total to be paid for houly
-    def total_from_weekly_pay(self,employee,hours_worked):
-        total_hourly_to_pay = Employee.get_hourly_rate(employee) * hours_worked
+    @staticmethod
+    def total_from_weekly_pay(employee,hours_worked):
+        total_hourly_to_pay = employee.get_hourly_rate() * hours_worked
         return total_hourly_to_pay
     
-    def final_weekly_pay(self):
-        
+    #Parametrs: self,Employee,total_hours_worked,total_products,total_sevices
+    #returns: the total to be paid to employee for weekely pay
+    @staticmethod
+    def final_weekly_pay(employee,hours_worked,total_products,_total_services):
+        #calculate the total hourly pay
+        total_hourly_to_pay = mathlogic.total_from_weekly_pay(employee,hours_worked)
+        #calculate the total commisson Services
+        total_commisson_services_made = mathlogic.total_made_commison_from_services(employee,_total_services)
+        #calculate the total commisson Products
+        total_commisson_products_made = mathlogic.total_made_commissom_from_products(employee,total_products)
+        final_weekly_pay_to_employee = total_hourly_to_pay + total_commisson_services_made + total_commisson_products_made
+        return final_weekly_pay_to_employee
+    
+    #want to come up with an algorthim / formula to determine how much the bussiness owner made after paying the employee
+    #this will be for records as well as to determine profit margins are being kept
